eafcbaa35107b7ef6a2808710119928ad9836199
Prateek Chhimwal <47253656+prateek1o1@users.noreply.github.com>
2023-03-15 16:08:49 +0530

    Update Calculator.java

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -18,12 +18,14 @@ public class Calculator {
         double input1,input2;
         do{
             System.out.println("Hello There! What a nice day to learn about devops pipeline through this mini-project.\nEnter your choice-");
-            System.out.print("Press 1 to find Square Root\n" +
-                    "Press 2 to find Factorial \n" +
-                    "Press 3 to find Natural Logarithm \n" +
-                    "Press 4 to find Power \n" +
-                    "Press 5 to exit \n" +
-                    "Enter your choice:");
+            System.out.print("""
+                    Press 1 to find Square Root
+                    Press 2 to find Factorial
+                    Press 3 to find Natural Logarithm
+                    Press 4 to find Power
+                    Press 5 to exit
+                    Enter your choice:\s""");
+
             int key;
             try{
                 key= scan.nextInt();
@@ -31,18 +33,44 @@ public class Calculator {
             catch(InputMismatchException err){
                 return;
             }
-            switch (key){
-                case 1:
+
+            switch (key) {
+                case 1 -> {
+                    //Square Root
                     System.out.print("Give your input-");
                     input1 = scan.nextDouble();
                     System.out.println("Square Root of " + input1 + " is : " + object.squareroot(input1));
                     System.out.println("\n");
-                    break;
-                default:
-                    System.out.println("Signing Off!");
+                }
+                case 2 -> {
+                    //Factorial Function
+                    System.out.print("Give your input-");
+                    input1 = scan.nextDouble();
+                    System.out.println("Factorial of " + input1 + " is : " + object.factorial(input1));
+                    System.out.println("\n");
+                }
+                case 3 -> {
+                    //Natural Logarithm
+                    System.out.print("Give your input-");
+                    input1 = scan.nextDouble();
+                    System.out.println("Natural Logarithm of " + input1 + " is : " + object.logarithm(input1));
+                    System.out.println("\n");
+                }
+                case 4 -> {
+                    //Power Function
+                    System.out.print("Give your first input-");
+                    input1 = scan.nextDouble();
+                    System.out.print("Give your second input-");
+                    input2 = scan.nextDouble();
+                    System.out.println(" " + input1 + " to the power of " + input2 + " is : " + object.power(input1, input2));
+                    System.out.println("\n");
+                }
+                default -> {
+                    System.out.println("Signing off!");
                     return;
+                }
             }
-        }while(true);
+        }while (true);
     }
     public double squareroot(double input1) {
         logger.info("[SQUARE ROOT] - " + input1);
@@ -50,4 +78,28 @@ public class Calculator {
         logger.info("[RESULT - SQUARE ROOT] - " + result);
         return result;
     }
+    public Double factorial(double input1) {
+        logger.info("[FACTORIAL] - " + input1);
+        double result = 1.0;
+        for (int i = 2; i <= input1; i++)
+            result = result*i;
+
+        logger.info("[RESULT - FACTORIAL] - " + result);
+        return result;
+    }
+
+    public double logarithm(double input1) {
+        logger.info("[LOGARITHM] - " + input1);
+        double result = Math.log(input1);
+        logger.info("[RESULT - LOGARITHM] - " + result);
+        return result;
+    }
+    public double power(double input1,double input2) {
+        logger.info("[POWER] - " + input1);
+        double result = Math.pow(input1,input2);
+        logger.info("[RESULT - POWER] - " + result);
+        return result;
+    }
+
+
 }
